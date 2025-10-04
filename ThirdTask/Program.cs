0320cb30c6b11819a86c5d5d33bf3c44dded60d4
aaaalfo <antonmaygkov30@gmail.com>
2025-10-05 00:48:55 +0300

    T

---
+++ ThirdTask/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        
+        int secretNumber = random.Next(1, 11);
+        int userGuess;
+        
+        do
+        {
+
+            string input = Console.ReadLine();
+            
+            if (int.TryParse(input, out userGuess))
+            {
+                
+                if (userGuess == secretNumber)
+                {
+                    Console.WriteLine($"Поздравляем! Вы угадали число {secretNumber}!");
+                }
+                else if (userGuess < secretNumber)
+                {
+                    Console.WriteLine("Загаданное число больше");
+                }
+                else
+                {
+                    Console.WriteLine("Загаданное число меньше");
+                }
+            }
+            
+        } while (userGuess != secretNumber);
+    }
+}
