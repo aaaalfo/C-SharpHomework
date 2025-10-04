365f6c00cadbfc6dd9359b10e980f09f51a3dc5f
aaaalfo <antonmaygkov30@gmail.com>
2025-10-05 00:30:41 +0300

    S

---
+++ SecondTask/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double sum = 0;
+        
+        
+        while (true)
+        {
+
+            string input = Console.ReadLine();
+            
+            if (double.TryParse(input, out double number))
+            {
+                if (number == 0)
+                {
+                    break;
+                }
+
+                sum += number;
+                Console.WriteLine(sum);
+            }
+        }
+        
+        Console.WriteLine(sum);
+    }
+}
