03505d290a9047b713eae912b6ac8c2afc79e48d
aaaalfo <antonmaygkov30@gmail.com>
2025-10-05 00:22:35 +0300

    F

---
+++ FirstTask/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        double celsius = Convert.ToDouble(Console.ReadLine());
+        double fahrenheit = (celsius * 9 / 5) + 32;
+        
+        Console.WriteLine($"{celsius}°C = {fahrenheit}°F");
+    }
+}
