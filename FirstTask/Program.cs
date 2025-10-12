d3f1a05f013e736bc725c81b10c2580ae9299c77
aaaalfo <antonmaygkov30@gmail.com>
2025-10-12 15:52:58 +0300

    FirstTask

--- FirstTask/Program.cs
+++ FirstTask/Program.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main()
     {
+        string input = Console.ReadLine();
+        int celsius;
+        bool isSuccess = int.TryParse(input, out celsius);
+        if (!isSuccess)
+        {
+            Console.WriteLine("Неверный формат числа!");
+            Environment.Exit(0);
+        }
 
-        double celsius = Convert.ToDouble(Console.ReadLine());
         double fahrenheit = (celsius * 9 / 5) + 32;
         
         Console.WriteLine($"{celsius}°C = {fahrenheit}°F");
