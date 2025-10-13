a8743f797a6088ff6f83fb7dcc5f6754977489e1
aaaalfo <antonmaygkov30@gmail.com>
2025-10-13 16:25:20 +0300

    ForthTask

--- ForthTask/Program.cs
+++ ForthTask/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Formats.Asn1;
 
 namespace ForthTask
 {
@@ -6,15 +7,26 @@ namespace ForthTask
     {
         static void Main()
         {
+
+            Console.WriteLine("Введите пароль. Он должен удовлетворять следующим критериям:");
+            Console.WriteLine("● Длина не менее 8 символов");
+            Console.WriteLine("● Содержит хотя бы одну цифру");
+            Console.WriteLine("● Содержит хотя бы одну заглавную букву");
+            Console.WriteLine("● Содержит хотя бы один специальный символ (!@#$%^&*)");
+
             string password = Console.ReadLine();
+
             string specialCharacters = "!@#$%^&*";
+
             bool digitFlag = false;
             bool uppercaseFlag = false;
             bool specialFlag = false;
+            
             int passwordLength = password.Length;
+
             if (passwordLength < 8)
             {
-                Console.WriteLine("Первый критерий не выполнен");
+                Console.WriteLine("● Первый критерий не выполнен");
             }
             foreach (char chr in password)
             {
@@ -33,19 +45,20 @@ namespace ForthTask
             }
             if (!digitFlag)
             {
-                Console.WriteLine("Второй критерий не выполнен");
+                Console.WriteLine("● Второй критерий не выполнен");
             }
             if (!uppercaseFlag)
             {
-                Console.WriteLine("Третий критерий не выполнен");
+                Console.WriteLine("● Третий критерий не выполнен");
             }
             if (!specialFlag)
             {
-                Console.WriteLine("Четвертый критерий не выполнен");
+                Console.WriteLine("● Четвертый критерий не выполнен");
             }
-            else
+
+            else if (specialFlag && uppercaseFlag && digitFlag && passwordLength >= 8) 
             {
-                Console.WriteLine("Пароль удовлетворяет всем критериям");
+                Console.WriteLine("Пароль удовлетворяет всем критериям!");
             }
             
         }
